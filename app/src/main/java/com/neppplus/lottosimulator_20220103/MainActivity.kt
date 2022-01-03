b0eb37ac6cb28c3dc664a88b862c6f192e1cc5f4
ujin-Bang <qkddnwls14@gmail.com>
2022-01-03 18:29:26 +0900

    Initial commit

---
+++ app/src/main/java/com/neppplus/lottosimulator_20220103/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.neppplus.lottosimulator_20220103
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
