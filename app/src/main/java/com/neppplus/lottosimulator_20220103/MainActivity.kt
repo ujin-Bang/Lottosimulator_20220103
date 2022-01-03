0154bdf827dd5ab8ca320f12543c31811c474915
ujin-Bang <qkddnwls14@gmail.com>
2022-01-03 23:15:09 +0900

    등수 확인 로그  - 얼마 사용했고 / 얼마 당첨? 표시 해야 함

--- app/src/main/java/com/neppplus/lottosimulator_20220103/MainActivity.kt
+++ app/src/main/java/com/neppplus/lottosimulator_20220103/MainActivity.kt
@@ -55,6 +55,32 @@ class MainActivity : AppCompatActivity() {
             }
         }
         Log.d("맞춘 갯수","${correctCount}개 맞춤")
+        //등수 판단 - when 활용
+
+        when(correctCount) {
+            6 -> {
+                Log.d("등수","1등입니다")
+            }
+            5 -> {
+                //보너스 번호 검사 -> 보너스 번호가 내 번호안에 있는가?
+                if(mMyLottoNumArr.contains(mBonusNum)){
+                    Log.d("등수","2등")
+                }
+                else{
+                    Log.d("등수","3등")
+                }
+            }
+            4 -> {
+                Log.d("등수","4등")
+            }
+            3 -> {
+                Log.d("등수","5등")
+            }
+            else -> {
+                Log.d("등수","꽝 다음기회에..")
+            }
+        }
+
     }
 
     fun makeBonusNum() {
